a51749552cda62c2748798ff703c2cae5d599a39
Viet Anh <tavietanh16112004@gmail.com>
2025-06-10 01:42:34 +0000

    update daily problem

---
+++ daily/09_06_2025/solution.py
@@ -0,0 +1,14 @@
+# LeetCode Daily Challenge (2025-06-09)
+# Title: K-th Smallest in Lexicographical Order
+# Difficulty: Hard
+# URL: https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/
+#
+# Given two integers n and k, return the kth lexicographically smallest integer in the range [1, n].
+# 
+#  
+
+
+# Your solution starts here
+
+def solution():
+    pass
