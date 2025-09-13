4924b525bdb869e95970b974ad8dde35afea5a35
Viet Anh <tavietanh16112004@gmail.com>
2025-09-13 01:29:21 +0000

    update daily problem

--- daily/02_09_2025/solution.py
+++ daily/02_09_2025/solution.py
@@ -4,21 +4,22 @@
 # URL: https://leetcode.com/problems/find-the-number-of-ways-to-place-people-i/
 #
 # You are given a 2D array points of size n x 2 representing integer coordinates of some points on a 2D plane, where points[i] = [xi, yi].
-# 
+#
 # Count the number of pairs of points (A, B), where
-# 
-# 
+#
+#
 # 	A is on the upper left side of B, and
 # 	there are no other points in the rectangle (or line) they make (including the border).
-# 
-# 
+#
+#
 # Return the count.
-# 
-#  
+#
+#
 
 
 # Your solution starts here
 from typing import List
+
 
 class Solution:
     def numberOfPairs(self, points: List[List[int]]) -> int:
@@ -34,17 +35,23 @@ class Solution:
             for j in range(i + 1, n):
                 first_point = points[i]
                 second_point = points[j]
-                if second_point[0] >= first_point[0] and second_point[1] <= first_point[1]:
+                if (
+                    second_point[0] >= first_point[0]
+                    and second_point[1] <= first_point[1]
+                ):
                     valid_pairs.append((first_point, second_point))
-                
-                if first_point[0] >= second_point[0] and first_point[1] <= second_point[1]:
+
+                if (
+                    first_point[0] >= second_point[0]
+                    and first_point[1] <= second_point[1]
+                ):
                     valid_pairs.append((second_point, first_point))
-        
+
         final_valid_pairs = []
 
         for i in range(len(valid_pairs)):
-            x1, y1 = valid_pairs[i][0] # point A
-            x2, y2 = valid_pairs[i][1] # point B
+            x1, y1 = valid_pairs[i][0]  # point A
+            x2, y2 = valid_pairs[i][1]  # point B
             is_valid = True
             for j in range(n):
                 if points[j] == valid_pairs[i][0] or points[j] == valid_pairs[i][1]:
@@ -53,10 +60,10 @@ class Solution:
                     if x1 <= points[j][0] <= x2 and y2 <= points[j][1] <= y1:
                         is_valid = False
                         break
-            
+
             if is_valid:
-                    final_valid_pairs.append(valid_pairs[i])
-        
+                final_valid_pairs.append(valid_pairs[i])
+
         # Time Complexity: O(n^2)
         # Space Complexity: O(n^2)
         return len(final_valid_pairs)
---
+++ daily/13_09_2025/solution.py
@@ -0,0 +1,28 @@
+# LeetCode Daily Challenge ()
+# Title:
+# Difficulty:
+# URL: https://leetcode.com/problems//
+#
+#
+
+
+# Your solution starts here
+from collections import defaultdict
+
+
+class Solution:
+    def maxFreqSum(self, s: str) -> int:
+        vowels = ["a", "e", "i", "o", "u"]
+
+        freq = defaultdict(int)
+        max_vowel = 0
+        max_consonant = 0
+
+        for char in s:
+            freq[char] += 1
+            if char in vowels:
+                max_vowel = max(max_vowel, freq[char])
+            else:
+                max_consonant = max(max_consonant, freq[char])
+
+        return max_vowel + max_consonant
