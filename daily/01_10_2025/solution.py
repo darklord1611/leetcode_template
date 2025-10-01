7c04c929abc93f19131036baa8ea51a147d4214e
Viet Anh <tavietanh16112004@gmail.com>
2025-10-01 10:25:47 +0000

    update daily problem

---
+++ daily/01_10_2025/solution.py
@@ -0,0 +1,21 @@
+# LeetCode Daily Challenge ()
+# Title:
+# Difficulty:
+# URL: https://leetcode.com/problems//
+#
+#
+
+
+# Your solution starts here
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        max_bottle = numBottles
+
+        empty = numBottles
+
+        while empty >= numExchange:
+            max_bottle += empty // numExchange
+
+            empty = empty // numExchange + empty % numExchange
+
+        return max_bottle
