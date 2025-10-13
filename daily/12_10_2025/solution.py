1ead951576bc455f40b33ab1849afed34cde8861
Viet Anh <tavietanh16112004@gmail.com>
2025-10-13 01:04:39 +0000

    update daily problem

---
+++ daily/12_10_2025/solution.py
@@ -0,0 +1,30 @@
+# LeetCode Daily Challenge (2025-10-12)
+# Title: Find Sum of Array Product of Magical Sequences
+# Difficulty: Hard
+# URL: https://leetcode.com/problems/find-sum-of-array-product-of-magical-sequences/
+#
+# You are given two integers, m and k, and an integer array nums.
+# A sequence of integers seq is called magical if:
+#
+#
+# 	seq has a size of m.
+# 	0 &lt;= seq[i] &lt; nums.length
+# 	The binary representation of 2seq[0] + 2seq[1] + ... + 2seq[m - 1] has k set bits.
+#
+#
+# The array product of this sequence is defined as prod(seq) = (nums[seq[0]] * nums[seq[1]] * ... * nums[seq[m - 1]]).
+#
+# Return the sum of the array products for all valid magical sequences.
+#
+# Since the answer may be large, return it modulo 109 + 7.
+#
+# A set bit refers to a bit in the binary representation of a number that has a value of 1.
+#
+#
+
+
+# Your solution starts here
+
+
+def solution():
+    pass
---
+++ daily/13_10_2025/solution.py
@@ -0,0 +1,36 @@
+# LeetCode Daily Challenge (2025-10-13)
+# Title: Find Resultant Array After Removing Anagrams
+# Difficulty: Easy
+# URL: https://leetcode.com/problems/find-resultant-array-after-removing-anagrams/
+#
+# You are given a 0-indexed string array words, where words[i] consists of lowercase English letters.
+#
+# In one operation, select any index i such that 0 &lt; i &lt; words.length and words[i - 1] and words[i] are anagrams, and delete words[i] from words. Keep performing this operation as long as you can select an index that satisfies the conditions.
+#
+# Return words after performing all operations. It can be shown that selecting the indices for each operation in any arbitrary order will lead to the same result.
+#
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase using all the original letters exactly once. For example, &quot;dacb&quot; is an anagram of &quot;abdc&quot;.
+#
+#
+
+
+# Your solution starts here
+from typing import List
+
+
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        # anagrams when sorted would result in the same string
+        # loop through
+
+        n = len(words)
+        mask = [1 for _ in range(n)]
+        prev_idx = 0
+
+        for i in range(1, n):
+            if "".join(sorted(words[i])) == "".join(sorted(words[prev_idx])):
+                mask[i] = 0
+            else:
+                prev_idx = i
+
+        return [words[i] for i in range(n) if mask[i] == 1]
