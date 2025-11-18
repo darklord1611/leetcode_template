e78a6245d6e6d26f21016c8938d9cc42d20c827f
Viet Anh <tavietanh16112004@gmail.com>
2025-11-18 10:29:44 +0000

    update daily problem

--- leetcode_daily/17_11_2025/solution.py
+++ leetcode_daily/17_11_2025/solution.py
@@ -1,10 +1,11 @@
 # LeetCode Daily Challenge (2025-11-17)\n# Title: Check If All 1's Are at Least Length K Places Away\n# Difficulty: Easy\n# Acceptance Rate: 59.093708117506424\n# Tags: Array\n# URL: https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away/\n#\n# Given an binary array nums and an integer k, return true if all 1&#39;s are at least k places away from each other, otherwise return false.
-# 
-#  
+#
+#
 
 
 # Your solution starts here
 from typing import List
+
 
 class Solution:
     def kLengthApart(self, nums: List[int], k: int) -> bool:
@@ -18,10 +19,10 @@ class Solution:
                 prev = cur
                 cur = i
 
-                if prev == -1: # we haven't reach the second 1s yet
+                if prev == -1:  # we haven't reach the second 1s yet
                     continue
 
                 if cur - prev <= k:
                     return False
-        
+
         return True
---
+++ leetcode_daily/18_11_2025/solution.py
@@ -0,0 +1,36 @@
+# LeetCode Daily Challenge (2025-11-18)\n# Title: 1-bit and 2-bit Characters\n# Difficulty: Easy\n# Acceptance Rate: 46.18478025832108\n# Tags: Array\n# URL: https://leetcode.com/problems/1-bit-and-2-bit-characters/\n#\n# We have two special characters:
+#
+#
+# 	The first character can be represented by one bit 0.
+# 	The second character can be represented by two bits (10 or 11).
+#
+#
+# Given a binary array bits that ends with 0, return true if the last character must be a one-bit character.
+#
+#
+
+
+# Your solution starts here
+
+from typing import List
+
+
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        # 10101011010001110
+
+        # when do we have valid one-bit character at the end -> when we already process m two-bit characters and n one-bit characters before that
+        # meaning that if we are able to combine the last and the second-to-last character to be two-bit character, we would have ended up at index k - 1
+
+        n = len(bits)
+        idx = 0
+
+        while idx < n:
+            if bits[idx] == 0:
+                if idx == n - 1:
+                    return True
+                idx += 1
+            else:
+                idx += 2
+
+        return False
