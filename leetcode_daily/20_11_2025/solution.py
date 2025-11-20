0924e83911a3b982f9cb7b9474179e5cb94e6108
Viet Anh <tavietanh16112004@gmail.com>
2025-11-20 07:24:56 +0000

    update daily problem

---
+++ leetcode_daily/20_11_2025/solution.py
@@ -0,0 +1,16 @@
+# LeetCode Daily Challenge (2025-11-20)\n# Title: Set Intersection Size At Least Two\n# Difficulty: Hard\n# Acceptance Rate: 51.35271171511262\n# Tags: Array, Greedy, Sorting\n# URL: https://leetcode.com/problems/set-intersection-size-at-least-two/\n#\n# You are given a 2D integer array intervals where intervals[i] = [starti, endi] represents all the integers from starti to endi inclusively.
+#
+# A containing set is an array nums where each interval from intervals has at least two integers in nums.
+#
+#
+# 	For example, if intervals = [[1,3], [3,7], [8,9]], then [1,2,4,7,8,9] and [2,3,4,8,9] are containing sets.
+#
+#
+# Return the minimum possible size of a containing set.
+#
+#
+
+
+# Your solution starts here
+def solution():
+    pass
---
+++ leetgpu_daily/2025-11-20/cuda_example.cu
@@ -0,0 +1,21 @@
+#include <cuda_runtime.h>
+#include <iostream>
+
+__global__ void count_equal_kernel(const int* input, int* output, int N, int K) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx < N) {
+        if (input[idx] == K) {
+            atomicAdd(output, 1);
+        }
+    }
+}
+
+// input, output are device pointers (i.e. pointers to memory on the GPU)
+extern "C" void solve(const int* input, int* output, int N, int K) {
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+
+    count_equal_kernel<<<blocksPerGrid, threadsPerBlock>>>(input, output, N, K);
+    cudaDeviceSynchronize();
+}
